d9c19eb0eb13d1f49ba82f40fc53b018c5dc39b4
ThijsKwakernaak <Thijs.Kwakernaak@infosupport.com>
2024-10-20 19:39:27 +0200

    Add project files.

---
+++ Quacklibs.AtticOrganizer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Quacklibs.AtticOrganizer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Quacklibs.AtticOrganizer/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Quacklibs.AtticOrganizer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
